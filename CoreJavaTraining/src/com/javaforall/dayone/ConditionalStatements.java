a91385d0cd4f6d9046172c0f1ae66dd9fcb51d50
Anand <anand.tummapudi@gmail.com>
2023-11-04 19:37:39 -0700

    Adding day one practice programs

---
+++ CoreJavaTraining/src/com/javaforall/dayone/ConditionalStatements.java
@@ -0,0 +1,10 @@
+package com.javaforall.dayone;
+
+public class ConditionalStatements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CoreJavaTraining/src/com/javaforall/dayone/Constructors.java
@@ -0,0 +1,5 @@
+package com.javaforall.dayone;
+
+public class Constructors {
+
+}
